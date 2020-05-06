86cd8cf23932894881a01f230340531e74ff7efa
mikebrady62 <mikebrady62>
2020-05-06 17:53:55 -0400

    Added bin files

---
+++ bin/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install nodejs
+sudo apt install php
+wget https://www.apachefriends.org/xampp-files/7.1.10/xampp-linux-x64-7.1.10-0-installer.run
+chmod +x xampp-linux-x64-7.1.10-0-installer.run
+sudo ./xampp-linux-x64-7.1.10-0-installer.run
---
+++ bin/start_xampp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#sudo /opt/lampp/manager-linux-x64.run
+sudo /etc/init.d/apache2 stop
+sudo /opt/lampp/lampp start
